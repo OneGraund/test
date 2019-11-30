48f52ccd0f05e0242a24f404cfeabedba6f73e6d
Volodymyr Skoriy <48771478+OneGraund@users.noreply.github.com>
2019-11-30 10:34:05 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,7 @@
 import importlib
-import calculation
-from math import *
-
-#--------------------------------------Restricter-----------------------------
-def check_(string):
-    prohibited = ['import', '__', 'lambda', 'for', 'ALL_CLASSES', 'class', 'in ']
-    for el in prohibited:
-        if el in string:
-            return False
-
-def restic(askstring):
-    if check_(askstring) == False:
-        return False
-#----------------------------------------Writing-&-runing-function------------
-def wo_f(string):
-    with open("calculation.py", "w")as file:
-        f_t_w = f'from math import *\ndef calcul():\n\ttry:\n\t\ta={string}\n\texcept Exception as e:\n\t\tprint(e)\n\t\treturn\n\treturn a'
-        file.write(f_t_w)
-    importlib.reload(calculation)
-    result = calculation.calcul()
-    if result != None:
-        print(result)
-#----------------------------------------------------------------------------
+from restrictions import restic
+from write_open import wo_f
+result = None
 def main_func():
     askstring = input('Enter your example\n--->')
 #-----------------------------------------------------------
