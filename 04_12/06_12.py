643b3efc1c963bae9e629d265348bbbb42038fa8
Volodymyr Skoriy <48771478+OneGraund@users.noreply.github.com>
2019-12-06 17:23:41 +0200

    Add files via upload

---
+++ 04_12/06_12.py
@@ -0,0 +1,66 @@
+class Notebook:
+    def __init__(self, processor, graph_processor, hdd, mother_board, battery, corpus, display, keyboard, info):
+        pass
+
+class Processor:
+    def __init__(self, cores_amount, frequency, cash_size):
+        self.core_amount = core_amount
+        self.frequency = frequency
+        self.cash_size = cash_size
+
+class Graph_processor:
+    def __init__(self, frequency, video_memory_size):
+        self.frequency = frequency
+        self.video_memory_size = video_memory_size
+
+class HDD:
+    def __init__(self, motor_velocity, memory_size):
+        self.motor_velocity = motor_velocity
+        self.memory_size = memory_size
+
+class Mother_board:
+    def __init__(self, socket, chipset, front_side_bus):
+        self.socket = socket
+        self.chipset = chipset
+        self.front_side_bus = front_side_bus
+
+class Battery:
+    def __init__(self, capacity, material_type, power):
+        self.capacity = capacity
+        self.material_type = material_type
+        self.power = power
+
+class Corpus:
+    def __init__(self, colour, material, hightness, lenght_horizontal, lenght_vertical):
+        self.colour = colour
+        self.material = material
+        self.hightness = hightness
+        self.lenght_horizontal = lenght_horizontal
+        self.lenght_vertical = lenght_vertical
+
+class Display:
+    def __init__(self, diagonal, matrix_type, density):
+        self.diagonal = diagonal
+        self.matrix_type = matrix_type
+        self.density = density
+
+class Keyboard:
+    def __init__(self, key_travel, keyboard_type):
+        self.key_travel = key_travel
+        self.keyboard_type = keyboard_type
+
+class Info:
+    def __init__(self, firm, seller, serial_numer, description):
+        self.firm = firm
+        self.seller = seller
+        self.serial_numer = serial_number
+        self.description = descripton
+
+
+#-----------------------------------------------------------------------------------
+
+class Asus:
+    def __init__(self):
+        pass
+
+
