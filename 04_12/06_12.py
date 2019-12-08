036b4439272206ba13b70fdf0075df1d87789d0c
Dmytro <dkovtonjuk@gmail.com>
2019-12-08 12:57:22 +0200

    08-12 homework

--- 04_12/06_12.py
+++ 04_12/06_12.py
@@ -1,10 +1,6 @@
-class Notebook:
-    def __init__(self, processor, graph_processor, hdd, mother_board, battery, corpus, display, keyboard, info):
-        pass
-
 class Processor:
     def __init__(self, cores_amount, frequency, cash_size):
-        self.core_amount = core_amount
+        self.cores_amount = cores_amount
         self.frequency = frequency
         self.cash_size = cash_size
 
@@ -30,7 +26,7 @@ class Battery:
         self.material_type = material_type
         self.power = power
 
-class Corpus:
+class Case:
     def __init__(self, colour, material, hightness, lenght_horizontal, lenght_vertical):
         self.colour = colour
         self.material = material
@@ -50,17 +46,33 @@ class Keyboard:
         self.keyboard_type = keyboard_type
 
 class Info:
-    def __init__(self, firm, seller, serial_numer, description):
+    def __init__(self, firm, seller, serial_number, description):
         self.firm = firm
         self.seller = seller
-        self.serial_numer = serial_number
-        self.description = descripton
+        self.serial_number = serial_number
+        self.description = description
 
 
 #-----------------------------------------------------------------------------------
 
-class Asus:
-    def __init__(self):
-        pass
+class Notebook:
+    def __init__(self, processor, graph_processor, hdd, mother_board, battery, case, display, keyboard, info):
+        self.processor = processor
+        self.graph_processor = graph_processor
+        self.hdd = hdd
+        self.mother_board = mother_board
+        self.battery = battery
+        self.case = case
+        self.display = display
+        self.keyboard = keyboard
+        self.info = info
 
-
+asus = Notebook(processor = Processor(cores_amount=2, frequency=4.5, cash_size=50),
+    graph_processor = Graph_processor(frequency=1.2, video_memory_size=2),
+    hdd = HDD(motor_velocity=7600, memory_size=960),
+    mother_board = Mother_board(socket="AM4", chipset="H110", front_side_bus=256),
+    battery = Battery(capacity=2600, material_type="li-on", power=80),
+    case = Case(colour="black", material="aluminium, plastic", hightness=3, lenght_horizontal=40, lenght_vertical=25),
+    display = Display(diagonal=15.6, matrix_type="TS", density="1280x720"),
+    keyboard = Keyboard(key_travel=0.4, keyboard_type="membran"),
+    info = Info(firm="ASUS", seller="Rozetka", serial_number="C12353MN", description="Best notebook for 300 hohlo-baksiv"))
